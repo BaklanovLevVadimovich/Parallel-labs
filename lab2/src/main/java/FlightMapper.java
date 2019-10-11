0e2723c4b8dd555158983af2b897ac105e07b9ff
Baklanov Lev <penachett@bk.ru>
2019-10-11 22:29:03 +0300

    Scripted auto-commit on change (2019-10-11 22:29:03) by gitwatch.sh

--- lab2/src/main/java/FlightMapper.java
+++ lab2/src/main/java/FlightMapper.java
@@ -8,13 +8,14 @@ import java.io.IOException;
 public class FlightMapper extends Mapper<LongWritable, Text, JoinWritableComparable, Text> {
 
     private final int FLIGHT_TYPE = 1;
+    private final int DEST_AIRPORT_INDEX = 14;
 
     @Override
 
     protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
 
         if (!key.equals(new LongWritable(0))) {
-                        
+
         }
     }
 
