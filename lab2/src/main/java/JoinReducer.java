f8afeeddddc78ed540e116f0dee67d6ca4430d52
Baklanov Lev <penachett@bk.ru>
2019-10-11 22:56:47 +0300

    Scripted auto-commit on change (2019-10-11 22:56:46) by gitwatch.sh

--- lab2/src/main/java/JoinReducer.java
+++ lab2/src/main/java/JoinReducer.java
@@ -9,15 +9,9 @@ import java.util.Iterator;
 public class JoinReducer extends Reducer<JoinWritableComparable, Text, Text, Text> {
 
     @Override
-    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+    protected void reduce(JoinWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
-        long count=0;
-        Iterator iter = values.iterator();
-        while (iter.hasNext()) {
-            iter.next();
-            count++;
-        }
-        context.write(key, new LongWritable(count));
+        
     }
 
 } 
