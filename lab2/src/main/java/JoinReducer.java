615f53fe2cfd1fe16f1c44368f5e86a84e3965c0
Baklanov Lev <penachett@bk.ru>
2019-10-11 22:55:34 +0300

    Scripted auto-commit on change (2019-10-11 22:55:34) by gitwatch.sh

--- lab2/src/main/java/JoinReducer.java
+++ lab2/src/main/java/JoinReducer.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 import java.util.Iterator;
 
-public class JoinReducer extends Reducer<JoinWritableComparable, Text, Text, LongWritable> {
+public class JoinReducer extends Reducer<JoinWritableComparable, Text, Text, Text> {
 
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
