88a1cee2a4185d00754abc989530b93e5506e42d
Baklanov Lev <penachett@bk.ru>
2019-10-11 22:25:33 +0300

    Scripted auto-commit on change (2019-10-11 22:25:33) by gitwatch.sh

--- lab2/src/main/java/FlightMapper.java
+++ lab2/src/main/java/FlightMapper.java
@@ -1,2 +1,21 @@
-public class FlightMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class FlightMapper extends Mapper<LongWritable, Text, JoinWritableComparable, Text> {
+
+    private final int AIRPORT_TYPE = 0;
+
+    @Override
+
+    protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
+
+        if (!key.equals(new LongWritable(0))) {
+            
+        }
+    }
+
 }
