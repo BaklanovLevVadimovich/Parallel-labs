64089399d8d17e3bd0c8f894994aa6fdef9ce4d9
Baklanov Lev <penachett@bk.ru>
2019-10-11 23:12:19 +0300

    Scripted auto-commit on change (2019-10-11 23:12:19) by gitwatch.sh

--- lab2/src/main/java/JoinReducer.java
+++ lab2/src/main/java/JoinReducer.java
@@ -41,7 +41,8 @@ public class JoinReducer extends Reducer<JoinWritableComparable, Text, Text, Tex
         averageDelay = delaySum/flightsDelayed;
 
         if (flightsDelayed > 0) {
-            context.write(new Text(airportName + ": "), new Text("average = " + String.format("")));
+            context.write(new Text(airportName + ": "), new Text("average delay = " + String.format("%.2f", averageDelay) +
+                    " , min delay = " + String.format("%.2f", minDelay) + " , max delay = " + String.format("%.2f", maxDelay)));
         }
     }
 
