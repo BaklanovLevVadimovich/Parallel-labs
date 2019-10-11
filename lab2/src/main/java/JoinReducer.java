b6004e41dccad95492a432d1b75a652a2b57ae8c
Baklanov Lev <penachett@bk.ru>
2019-10-11 23:02:54 +0300

    Scripted auto-commit on change (2019-10-11 23:02:54) by gitwatch.sh

--- lab2/src/main/java/JoinReducer.java
+++ lab2/src/main/java/JoinReducer.java
@@ -11,11 +11,18 @@ public class JoinReducer extends Reducer<JoinWritableComparable, Text, Text, Tex
     @Override
     protected void reduce(JoinWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Text airportName = new Text();
-        boolean firstLine = true;
+        boolean firstLineFlag = true;
         int flightsDelayed = 0;
         float delaySum = 0;
         float minDelay = -1;
         float maxDelay = -1;
+
+        Iterator iter = values.iterator();
+        for (iter.hasNext()) {
+            if (firstLineFlag) {
+                
+            }
+        }
     }
 
 } 
