cbae4ab5b76996fa9a82839488d6e4cdf0c1066f
Baklanov Lev <penachett@bk.ru>
2019-10-11 22:44:50 +0300

    Scripted auto-commit on change (2019-10-11 22:44:50) by gitwatch.sh

--- lab2/src/main/java/JoinPartitioner.java
+++ lab2/src/main/java/JoinPartitioner.java
@@ -1,7 +1,7 @@
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.Partitioner;
 
-public class JoinPartitioner implements Partitioner<JoinWritableComparable, Text> {
+public class JoinPartitioner extends Partitioner<JoinWritableComparable, Text> {
     @Override
     public int getPartition(JoinWritableComparable key, Text value, int reduceTasksNum) {
         return key.getAirportId() & reduceTasksNum;
