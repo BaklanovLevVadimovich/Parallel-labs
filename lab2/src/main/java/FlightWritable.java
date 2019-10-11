0eb192a142c274548a23461f2eb91967671d8d98
Baklanov Lev <penachett@bk.ru>
2019-10-11 20:43:35 +0300

    Scripted auto-commit on change (2019-10-11 20:43:35) by gitwatch.sh

--- lab2/src/main/java/FlightWritable.java
+++ lab2/src/main/java/FlightWritable.java
@@ -2,6 +2,7 @@ import org.apache.hadoop.io.Writable;
 
 import java.io.DataInput;
 import java.io.DataOutput;
+import java.io.IOException;
 
 public class FlightWritable implements Writable {
 
@@ -13,8 +14,13 @@ public class FlightWritable implements Writable {
 
     @Override
     public void write(DataOutput output) {
-        output.writeInt(airportId);
-        
+        try {
+            output.writeInt(airportId);
+            output.writeUTF(airportName);
+        } catch (IOException e) {
+            System.out.println("FlightWritable write exception");
+            e.printStackTrace();
+        }
     }
 
     @Override
