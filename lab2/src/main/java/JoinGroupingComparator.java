7bd4d63d129fc874d95b68c014addd3b8992fc8e
Baklanov Lev <penachett@bk.ru>
2019-10-11 22:51:03 +0300

    Scripted auto-commit on change (2019-10-11 22:51:03) by gitwatch.sh

--- lab2/src/main/java/JoinGroupingComparator.java
+++ lab2/src/main/java/JoinGroupingComparator.java
@@ -1,8 +1,8 @@
 import org.apache.hadoop.io.WritableComparator;
 
 public class JoinGroupingComparator extends WritableComparator {
-    @Override
-    public int compare(JoinWritableComparable first, JoinWritableComparable second) {
 
+    public int compare(JoinWritableComparable first, JoinWritableComparable second) {
+        return first.getAirportId() - second.getAirportId();
     }
 }
