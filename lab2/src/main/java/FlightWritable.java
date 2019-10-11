d7d95161cac88c4be01b9bdd09670e1a898a972b
Baklanov Lev <penachett@bk.ru>
2019-10-11 20:34:48 +0300

    Scripted auto-commit on change (2019-10-11 20:34:48) by gitwatch.sh

--- lab2/src/main/java/FlightWritable.java
+++ lab2/src/main/java/FlightWritable.java
@@ -8,6 +8,9 @@ public class FlightWritable implements Writable {
 
     @Override
     public void write(DataOutput dataOutput) {
-        
+
     }
+
+    @Override
+    public void readFields()
 }
