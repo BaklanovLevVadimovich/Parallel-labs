283982002ca1d843a5e2317fe6f2846e6651487a
Baklanov Lev <penachett@bk.ru>
2019-10-11 22:05:26 +0300

    Scripted auto-commit on change (2019-10-11 22:05:26) by gitwatch.sh

--- lab2/src/main/java/AirportMapper.java
+++ lab2/src/main/java/AirportMapper.java
@@ -1,2 +1,23 @@
-public class AirportMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+
+    protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
+
+        String line = value.toString();
+        line = line.replaceAll("[\\p{Punct}]", "");
+        String[] words = line.split(" ");
+        for (String word : words) {
+            word = word.toLowerCase();
+            context.write(new Text(word), new IntWritable(1));
+        }
+    }
+
 }
