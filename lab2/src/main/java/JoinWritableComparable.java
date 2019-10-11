1a374988f26733d625b495c1b1dfe162599c6f1d
Baklanov Lev <penachett@bk.ru>
2019-10-11 21:46:06 +0300

    Scripted auto-commit on change (2019-10-11 21:46:06) by gitwatch.sh

--- lab2/src/main/java/JoinWritableComparable.java
+++ lab2/src/main/java/JoinWritableComparable.java
@@ -1,6 +1,21 @@
 import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+
 public class JoinWritableComparable implements WritableComparable<JoinWritableComparable> {
 
 
+    @Override
+    public void write(DataOutput output) {
+
+    }
+
+    @Override
+    public void readFields(DataInput input) {
+
+    }
+
+    @Override
+    public 
 }
