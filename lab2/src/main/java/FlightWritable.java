cb0709745b245dadae19202fab7b4cd39e11f6eb
Baklanov Lev <penachett@bk.ru>
2019-10-11 20:41:44 +0300

    Scripted auto-commit on change (2019-10-11 20:41:44) by gitwatch.sh

--- lab2/src/main/java/FlightWritable.java
+++ lab2/src/main/java/FlightWritable.java
@@ -7,17 +7,34 @@ public class FlightWritable implements Writable {
 
     private int airportId;
 
-    private int airport
+    private String airportName;
 
 
 
     @Override
     public void write(DataOutput output) {
-        output.writeInt();
+        output.writeInt(airportId);
+        
     }
 
     @Override
     public void readFields(DataInput input) {
 
+    }
+
+    public int getAirportId() {
+        return airportId;
+    }
+
+    public String getAirportName() {
+        return airportName;
+    }
+
+    public void setAirportId(int airportId) {
+        this.airportId = airportId;
+    }
+
+    public void setAirportName(String airportName) {
+        this.airportName = airportName;
     }
 }
