a1bcd7f7ecc2bde32364d96fcb6964faa20abcdf
Baklanov Lev <penachett@bk.ru>
2019-10-12 17:14:04 +0300

    Scripted auto-commit on change (2019-10-12 17:14:04) by gitwatch.sh

--- lab2/start.sh
+++ lab2/start.sh
@@ -1,4 +1,6 @@
 mvn package &&
 hadoop fs -rm -r -f output &&
 export HADOOP_CLASSPATH=target/lab2-1.0-SNAPSHOT.jar &&
-hadoop AirportJoinApp L_AIRPORT_ID.csv 664600583_T_ONTIME_sample.csv output
+hadoop AirportJoinApp L_AIRPORT_ID.csv 664600583_T_ONTIME_sample.csv output &&
+rm -rf output &&
+hadoop fs -copyToLocal output
