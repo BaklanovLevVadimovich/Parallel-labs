ed56a533364f3302717ac9c33b05c1996c204f4a
Baklanov Lev <penachett@bk.ru>
2019-10-12 10:29:04 +0300

    Scripted auto-commit on change (2019-10-12 10:29:04) by gitwatch.sh

--- lab2/src/main/java/AirportJoinApp.java
+++ lab2/src/main/java/AirportJoinApp.java
@@ -1,6 +1,8 @@
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.lib.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -28,8 +30,8 @@ public class AirportJoinApp {
 
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
 
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightMapper.class);
 
         job.setGroupingComparatorClass(JoinGroupingComparator.class);
 
