26764dbc422e71e0edd8fdf95786268c88bc8971
Baklanov Lev <penachett@bk.ru>
2019-10-18 22:10:25 +0300

    Scripted auto-commit on change (2019-10-18 22:10:25) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/lab3/Flight.java
+++ lab3/src/main/java/ru/bmstu/lab3/Flight.java
@@ -4,5 +4,11 @@ import java.io.Serializable;
 
 public class Flight implements Serializable {
 
-    
+    private int originAirportId;
+
+    private int destAirportId;
+
+    private boolean isCancelled;
+
+    private float 
 }
