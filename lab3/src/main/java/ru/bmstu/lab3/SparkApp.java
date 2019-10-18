be98bc408943c28be1e20dff080f7f7fdc8e7c4e
Baklanov Lev <penachett@bk.ru>
2019-10-18 22:01:18 +0300

    Scripted auto-commit on change (2019-10-18 22:01:18) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/lab3/SparkApp.java
+++ lab3/src/main/java/ru/bmstu/lab3/SparkApp.java
@@ -17,13 +17,13 @@ public class SparkApp {
         JavaRDD<String> airportsLines = sc.textFile("L_AIRPORT_ID.csv");
         airportsLines.
 
-        JavaPairRDD<Integer, String> airportsData = airportsLines.filter(s -> !s.contains("Code,Description"))
+        JavaPairRDD<Integer, String> airportsData = airportsLines
                 .mapToPair(s -> {
             s = s.replace("\"", "");
             String[] fields = s.split(",", 2);
             int id = Integer.parseInt(fields[0]);
             String name = fields[1];
             return new Tuple2<>(id, name);
-        })
+        });
     }
 }
