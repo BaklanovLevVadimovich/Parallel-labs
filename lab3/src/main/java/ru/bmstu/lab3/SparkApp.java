26a72b2566489d17623c7e6d8e5128a05fc5fc92
Baklanov Lev <penachett@bk.ru>
2019-10-18 23:49:54 +0300

    Scripted auto-commit on change (2019-10-18 23:49:54) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/lab3/SparkApp.java
+++ lab3/src/main/java/ru/bmstu/lab3/SparkApp.java
@@ -48,7 +48,7 @@ public class SparkApp {
         JavaPairRDD<Tuple2<Integer, Integer>, FlightStat> flightStats =
                 flightsData.combineByKey(
                         p -> new FlightStat(1, p.getDelay() == 0f ? 0 : 1, p.isCancelled() ? 1 : 0, p.getDelay()),
-                        
+                        (FlightStat, p) -> 
                 )
     }
 }
