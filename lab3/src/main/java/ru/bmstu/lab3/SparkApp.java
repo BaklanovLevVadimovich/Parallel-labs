d5611b75853c9802bff9a2c9dc95203bde3ad64e
Baklanov Lev <penachett@bk.ru>
2019-10-18 21:29:30 +0300

    Scripted auto-commit on change (2019-10-18 21:29:29) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/lab3/SparkApp.java
+++ lab3/src/main/java/ru/bmstu/lab3/SparkApp.java
@@ -6,6 +6,7 @@ public class SparkApp {
 
     public static void main(String[] args) {
 
-        SparkConf conf = new SparkConf()
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        
     }
 }
