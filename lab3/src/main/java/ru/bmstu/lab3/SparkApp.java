1bdc94619db18d21421a86f3821b211a485c9873
Baklanov Lev <penachett@bk.ru>
2019-10-19 00:05:45 +0300

    Scripted auto-commit on change (2019-10-19 00:05:45) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/lab3/SparkApp.java
+++ lab3/src/main/java/ru/bmstu/lab3/SparkApp.java
@@ -60,7 +60,7 @@ public class SparkApp {
         final Broadcast<Map<Integer, String>> airportsBroadcasted = sc.broadcast(airportsMap);
 
         JavaRDD<String> result = flightStats.map(
-                
+                s -> 
         )
 
     }
