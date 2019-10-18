6010f50c6a677d23bdced6663d80392db47ce010
Baklanov Lev <penachett@bk.ru>
2019-10-18 21:48:28 +0300

    Scripted auto-commit on change (2019-10-18 21:48:28) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/lab3/SparkApp.java
+++ lab3/src/main/java/ru/bmstu/lab3/SparkApp.java
@@ -1,6 +1,7 @@
 package ru.bmstu.lab3;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
@@ -14,7 +15,7 @@ public class SparkApp {
         JavaRDD<String> flightsLines = sc.textFile("664600583_T_ONTIME_sample.csv");
         JavaRDD<String> airportsLines = sc.textFile("L_AIRPORT_ID.csv");
 
-        JavaRDD<Integer, String> airportsData = airportsLines.mapToPair(s -> {
+        JavaPairRDD<Integer, String> airportsData = airportsLines.mapToPair(s -> {
             
         })
     }
