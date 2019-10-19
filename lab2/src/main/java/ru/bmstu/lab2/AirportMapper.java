07eef61a54461904edb9500e8da27160a0e3bcdf
Baklanov Lev <penachett@bk.ru>
2019-10-19 11:58:18 +0300

    Scripted auto-commit on change (2019-10-19 11:58:18) by gitwatch.sh

--- lab2/src/main/java/ru/bmstu/lab2/AirportMapper.java
+++ lab2/src/main/java/ru/bmstu/lab2/AirportMapper.java
@@ -12,13 +12,15 @@ public class AirportMapper extends Mapper<LongWritable, Text, JoinWritableCompar
     private static final int SPLIT_LIMIT = 2;
     private static final int ID_FIELD = 0;
     private static final int NAME_FIELD = 1;
+    private static final int FIRSTLINE_INDEX = 0;
     private static final String QUOTE_STRING = "\"";
     private static final String COMMA_STRING = ",";
+    private static final String EMPTY_STRING = "";
 
     @Override
     protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
 
-        if (!key.equals(new LongWritable(0))) {
+        if (!key.equals(new LongWritable(FIRSTLINE_INDEX))) {
             String line = value.toString();
             line = removeQuotes(line);
             String[] fields = line.split(COMMA_STRING, SPLIT_LIMIT);
@@ -30,7 +32,7 @@ public class AirportMapper extends Mapper<LongWritable, Text, JoinWritableCompar
     }
 
     private String removeQuotes(String line) {
-        return line.replace(QUOTE_STRING, "");
+        return line.replace(QUOTE_STRING, EMPTY_STRING);
     }
 
 }
