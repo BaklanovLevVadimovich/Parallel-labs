ec1ac44f465f6cb7fb956c8868e4b1ac0db93ec0
Baklanov Lev <penachett@bk.ru>
2019-11-22 01:59:39 +0300

    Scripted auto-commit on change (2019-11-22 01:59:39) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/akka_labs/StoreActor.java
+++ lab4/src/main/java/ru/bmstu/akka_labs/StoreActor.java
@@ -2,9 +2,12 @@ package ru.bmstu.akka_labs;
 
 import akka.actor.AbstractActor;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class StoreActor extends AbstractActor {
 
-    private Map<Integer, >
+    private Map<Integer, ArrayList<Test>>
 
     @Override
     public Receive createReceive() {
