a65a3ec12f683ca7c4992d5d9f0acfc5fe2cd6b7
Baklanov Lev <penachett@bk.ru>
2019-11-22 01:34:13 +0300

    Scripted auto-commit on change (2019-11-22 01:34:13) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/akka_labs/Test.java
+++ lab4/src/main/java/ru/bmstu/akka_labs/Test.java
@@ -3,5 +3,11 @@ package ru.bmstu.akka_labs;
 public class Test {
     private final String name;
     private final Object expectedResult;
-    private final Object[] 
+    private final Object[] params;
+
+    public Test(String name, Object expectedResult, Object[] params) {
+        this.name = name;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
 }
