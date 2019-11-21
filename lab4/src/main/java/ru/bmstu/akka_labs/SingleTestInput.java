9ddf069b3cccece1f53aaa547272aec891198b9c
Baklanov Lev <penachett@bk.ru>
2019-11-22 01:55:17 +0300

    Scripted auto-commit on change (2019-11-22 01:55:17) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/akka_labs/SingleTestInput.java
+++ lab4/src/main/java/ru/bmstu/akka_labs/SingleTestInput.java
@@ -1,4 +1,31 @@
 package ru.bmstu.akka_labs;
 
 public class SingleTestInput {
+    private final int packageId;
+    private final String jsScript;
+    private final String functionName;
+    private final Test test;
+
+    public SingleTestInput(int packageId, String jsScript, String functionName, Test test) {
+        this.packageId = packageId;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.test = test;
+    }
+
+    public int getPackageId() {
+        return packageId;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public String getJsScript() {
+        return jsScript;
+    }
+
+    public Test getTest() {
+        return test;
+    }
 }
