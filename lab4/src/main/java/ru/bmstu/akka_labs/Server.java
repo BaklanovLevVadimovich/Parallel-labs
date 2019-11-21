a4b6e9013eb1995b8d85ab1feacc045678b12aa9
Baklanov Lev <penachett@bk.ru>
2019-11-22 00:41:48 +0300

    Scripted auto-commit on change (2019-11-22 00:41:48) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/akka_labs/Server.java
+++ lab4/src/main/java/ru/bmstu/akka_labs/Server.java
@@ -17,7 +17,7 @@ public class Server {
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         Server instance = new Server();
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRoute().flow(system, materializer);
-        
+        final
     }
 
     private Route createRoute() {
