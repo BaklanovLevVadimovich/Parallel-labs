474712dfd1a870328e3e6e5329f763050fc3a29c
Baklanov Lev <penachett@bk.ru>
2019-11-22 01:57:16 +0300

    Scripted auto-commit on change (2019-11-22 01:57:16) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/akka_labs/RouteActor.java
+++ lab4/src/main/java/ru/bmstu/akka_labs/RouteActor.java
@@ -24,6 +24,7 @@ public class RouteActor extends AbstractActor {
                         worker.tell(new SingleTestInput(m.getPackageId(), m.getJsScript(), m.getFunctionName(), m.getTests()[i]), getSelf());
                     }
                 })
-                .match(Integer.class, m -> storage.tell(m, getSelf()));
+                .match(Integer.class, m -> storage.tell(m, getSelf()))
+                .build();
     }
 }
