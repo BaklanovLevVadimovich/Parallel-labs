e6ee714c9d7dbdb057a5c7e95d69249782ab9d84
Baklanov Lev <penachett@bk.ru>
2019-11-22 01:45:05 +0300

    Scripted auto-commit on change (2019-11-22 01:45:05) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/akka_labs/RouteActor.java
+++ lab4/src/main/java/ru/bmstu/akka_labs/RouteActor.java
@@ -1,10 +1,12 @@
 package ru.bmstu.akka_labs;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 
 public class RouteActor extends AbstractActor {
 
     private static final int ACTORS_NUM = 5;
+    private ActorRef 
 
     @Override
     public Receive createReceive() {
