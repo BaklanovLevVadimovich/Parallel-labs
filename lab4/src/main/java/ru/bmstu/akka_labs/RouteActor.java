c60c0091623ff9b409e08feb15a6a0756b60f837
Baklanov Lev <penachett@bk.ru>
2019-11-22 01:49:59 +0300

    Scripted auto-commit on change (2019-11-22 01:49:59) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/akka_labs/RouteActor.java
+++ lab4/src/main/java/ru/bmstu/akka_labs/RouteActor.java
@@ -19,6 +19,6 @@ public class RouteActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .match()
+                .match(Input)
     }
 }
