dcf028a85bfe3decd97c6083595a69593d31eb30
Baklanov Lev <penachett@bk.ru>
2019-11-22 00:49:07 +0300

    Scripted auto-commit on change (2019-11-22 00:49:07) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/akka_labs/Server.java
+++ lab4/src/main/java/ru/bmstu/akka_labs/Server.java
@@ -24,7 +24,7 @@ public class Server {
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow, ConnectHttp.toHost("localhost", 8081), materializer);
         System.out.println("Server online at localhost:8081");
         System.in.read();
-        binding.thenCompose(ServerBinding::unbind).thenAccept(unbound)
+        binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
     }
 
     private Route createRoute() {
