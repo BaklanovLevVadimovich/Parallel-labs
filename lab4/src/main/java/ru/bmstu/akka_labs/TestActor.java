5421e71efd51f695ecd181a673795377d1fb744d
Baklanov Lev <penachett@bk.ru>
2019-11-22 02:19:43 +0300

    Scripted auto-commit on change (2019-11-22 02:19:43) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/akka_labs/TestActor.java
+++ lab4/src/main/java/ru/bmstu/akka_labs/TestActor.java
@@ -2,10 +2,18 @@ package ru.bmstu.akka_labs;
 
 import akka.actor.AbstractActor;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 public class TestActor extends AbstractActor {
 
     private String runTest() {
-        
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval(jscript);
+        Invocable invocable = (Invocable) engine;
+        return invocable.invokeFunction(functionName, params).toString();
+
     }
 
     @Override
