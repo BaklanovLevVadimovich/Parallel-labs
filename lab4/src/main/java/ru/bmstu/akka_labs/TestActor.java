e096ea39071254f627a71a4587f5a9787758f73a
Baklanov Lev <penachett@bk.ru>
2019-11-22 02:21:47 +0300

    Scripted auto-commit on change (2019-11-22 02:21:47) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/akka_labs/TestActor.java
+++ lab4/src/main/java/ru/bmstu/akka_labs/TestActor.java
@@ -5,15 +5,15 @@ import akka.actor.AbstractActor;
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class TestActor extends AbstractActor {
 
-    private String runTest(SingleTestInput input) {
+    private String runTest(SingleTestInput input) throws ScriptException, NoSuchMethodException {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
         engine.eval(input.getJsScript());
         Invocable invocable = (Invocable) engine;
         return invocable.invokeFunction(input.getFunctionName(), input.getTest().getParams()).toString();
-
     }
 
     @Override
