31eb93aa8fa33437ebb78c4c3b0d61c19e3b2e17
Baklanov Lev <penachett@bk.ru>
2019-11-22 00:31:45 +0300

    Scripted auto-commit on change (2019-11-22 00:31:45) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/akka_labs/Server.java
+++ lab4/src/main/java/ru/bmstu/akka_labs/Server.java
@@ -2,12 +2,13 @@ package ru.bmstu.akka_labs;
 
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 public class Server {
 
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
-        
+        final ActorMaterializer 
     }
 }
