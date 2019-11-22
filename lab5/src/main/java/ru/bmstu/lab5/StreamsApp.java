7bbf2d66e8e68bb9ef82f3a009ebac4c29b2015c
Baklanov Lev <penachett@bk.ru>
2019-11-22 21:42:52 +0300

    Scripted auto-commit on change (2019-11-22 21:42:52) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/lab5/StreamsApp.java
+++ lab5/src/main/java/ru/bmstu/lab5/StreamsApp.java
@@ -56,7 +56,7 @@ public class StreamsApp {
 //                        }
 //                    });
                     Sink<Pair<String, Integer>, CompletionStage<Long>> innerSink = Flow.<Pair<String, Integer>>create()
-                            .mapConcat(pair -> )
+                            .mapConcat(p -> )
                 })
                 .map(res -> {
                     System.out.println("res = " + String.valueOf(res));
