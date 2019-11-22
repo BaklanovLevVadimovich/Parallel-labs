358364fd3bb4ddfecf5b34b1ffa83ab2b2e07f7b
Baklanov Lev <penachett@bk.ru>
2019-11-22 15:30:34 +0300

    Scripted auto-commit on change (2019-11-22 15:30:34) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/akka_labs/StoreActor.java
+++ lab4/src/main/java/ru/bmstu/akka_labs/StoreActor.java
@@ -25,7 +25,7 @@ public class StoreActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(SingleTestInput.class, m -> addTest(m.getPackageId(), m.getTest()))
-                .match(String.class, m -> sender().tell(new Result(Integer.parseInt(m), store.getOrDefault(Integer.parseInt(m))), getSelf()))
+                .match(String.class, m -> sender().tell(new Result(Integer.parseInt(m), store.getOrDefault(Integer.parseInt(m), new ArrayList<>())), getSelf()))
                 .build();
     }
 }
