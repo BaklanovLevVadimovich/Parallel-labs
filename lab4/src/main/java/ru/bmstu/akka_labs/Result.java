9fbbe8548f35224e9f0f351cd4ab658ab4356fa9
Baklanov Lev <penachett@bk.ru>
2019-11-22 16:21:23 +0300

    Scripted auto-commit on change (2019-11-22 16:21:23) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/akka_labs/Result.java
+++ lab4/src/main/java/ru/bmstu/akka_labs/Result.java
@@ -3,24 +3,15 @@ package ru.bmstu.akka_labs;
 import java.util.ArrayList;
 
 public class Result {
-    private int packageId;
     private ArrayList<Test> tests;
 
-    public Result(int packageId, ArrayList<Test> tests) {
-        this.packageId = packageId;
+    public Result(ArrayList<Test> tests) {
         this.tests = tests;
     }
 
-    public int getPackageId() {
-        return packageId;
-    }
 
     public ArrayList<Test> getTests() {
         return tests;
-    }
-
-    public void setPackageId(int packageId) {
-        this.packageId = packageId;
     }
 
     public void setTests(ArrayList<Test> tests) {
