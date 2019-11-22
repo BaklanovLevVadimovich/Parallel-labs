9bbe267574158ef9fafd056e7ddb15f1080cb385
Baklanov Lev <penachett@bk.ru>
2019-11-22 19:36:48 +0300

    Scripted auto-commit on change (2019-11-22 19:36:48) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/lab5/StreamsApp.java
+++ lab5/src/main/java/ru/bmstu/lab5/StreamsApp.java
@@ -1,10 +1,32 @@
 package ru.bmstu.lab5;
 
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 public class StreamsApp {
 
     public static void main(String[] args) throws IOException {
-        
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        StreamsApp instance = new StreamsApp();
+        ActorRef router = system.actorOf(Props.create(RouteActor.class));
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRoute(router).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow, ConnectHttp.toHost("localhost", 8081), materializer);
+        System.out.println("Server online at localhost:8081");
+        System.in.read();
+        binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
     }
 }
