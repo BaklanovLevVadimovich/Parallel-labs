e09c3f4b0315a7f7a0a88f13c8eea7f0e4a1103f
Baklanov Lev <penachett@bk.ru>
2019-11-23 00:10:03 +0300

    Scripted auto-commit on change (2019-11-23 00:10:03) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/lab5/StreamsApp.java
+++ lab5/src/main/java/ru/bmstu/lab5/StreamsApp.java
@@ -76,7 +76,7 @@ public class StreamsApp {
                                 })
                                 .toMat(Sink.fold(0L, Long::sum), Keep.right());
                         return Source.from(Collections.singletonList(pair))
-                                .toMat()
+                                .toMat(innerSink)
                     }
                 })
                 .map(res -> {
