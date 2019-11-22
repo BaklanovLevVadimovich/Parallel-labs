b5c234774cee90643f21ba6dc885e0ca86b86803
Baklanov Lev <penachett@bk.ru>
2019-11-23 00:31:33 +0300

    Scripted auto-commit on change (2019-11-23 00:31:33) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/lab5/StreamsApp.java
+++ lab5/src/main/java/ru/bmstu/lab5/StreamsApp.java
@@ -72,10 +72,7 @@ public class StreamsApp {
                                     return asyncHttpClient().prepareGet(p).execute().
                                             toCompletableFuture()
                                             .thenCompose(response ->
-                                            {
-                                                System.out.println((long)java.time.Duration.between(startTime, Instant.now()).getNano()/1000000);
-                                                return CompletableFuture.completedFuture((long)java.time.Duration.between(startTime, Instant.now()).getNano()/1000000);
-                                            });
+                                                    CompletableFuture.completedFuture((long)java.time.Duration.between(startTime, Instant.now()).getNano()/1000000));
                                 })
                                 .toMat(Sink.fold(0L, Long::sum), Keep.right());
                         return Source.from(Collections.singletonList(pair))
