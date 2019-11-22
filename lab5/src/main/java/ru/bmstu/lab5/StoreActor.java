4fb1e0f171d03e73ec5b0fe8ddbdb679539aad7e
Baklanov Lev <penachett@bk.ru>
2019-11-22 20:17:52 +0300

    Scripted auto-commit on change (2019-11-22 20:17:52) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/lab5/StoreActor.java
+++ lab5/src/main/java/ru/bmstu/lab5/StoreActor.java
@@ -17,18 +17,22 @@ public class StoreActor extends AbstractActor {
     private Long getResult(String url) {
         if (store.containsKey(url)) {
             System.out.println("Already got result for " + url);
-            return store.get(result.getUrl());
+            return store.get(url);
         } else {
-            System.out.println("No result for " + result.getUrl());
+            System.out.println("No result for " + url);
             return RESPONCE_TIME_DEFAULT_VALUE;
         }
+    }
+
+    private void saveResult(Result result) {
+        store.put(result.getUrl(), result.getResponseTime());
     }
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(SingleTestInput.class, m -> addTest(m.getPackageId(), m.getTest()))
-                .match(String.class, m -> sender().tell(new Result(store.getOrDefault(Integer.parseInt(m), new ArrayList<>())), getSelf()))
+                .match(Result.class, m -> saveResult(m))
+                .match(String.class, m -> sender().tell(getResult(m), getSelf()))
                 .build();
     }
 }
