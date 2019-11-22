e2cf3423f0ca9baf101c618636ad096bc14c8c7b
Baklanov Lev <penachett@bk.ru>
2019-11-22 19:39:06 +0300

    Scripted auto-commit on change (2019-11-22 19:39:06) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/lab5/StreamsApp.java
+++ lab5/src/main/java/ru/bmstu/lab5/StreamsApp.java
@@ -22,10 +22,8 @@ public class StreamsApp {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow;
-        StreamsApp instance = new StreamsApp();
-        ActorRef router = system.actorOf(Props.create(RouteActor.class));
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRoute(router).flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow, ConnectHttp.toHost("localhost", 8081), materializer);
+        StreamsApp instance = new StreamsApp();
         System.out.println("Server online at localhost:8081");
         System.in.read();
         binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
