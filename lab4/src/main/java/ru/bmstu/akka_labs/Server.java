730ef072a1c681d96f28830df7f6beb8b62523d6
Baklanov Lev <penachett@bk.ru>
2019-11-22 15:03:52 +0300

    Scripted auto-commit on change (2019-11-22 15:03:52) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/akka_labs/Server.java
+++ lab4/src/main/java/ru/bmstu/akka_labs/Server.java
@@ -49,7 +49,7 @@ public class Server {
                                         }))
                         )
                 ),
-                path("")
+                path("run", )
         );
     }
 }
