8398fbd764850c32338d1b9d28f3901f3c9eae79
Baklanov Lev <penachett@bk.ru>
2019-12-11 21:51:17 +0300

    Scripted auto-commit on change (2019-12-11 21:51:17) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/parallel/lab6/StoreActor.java
+++ lab6/src/main/java/ru/bmstu/parallel/lab6/StoreActor.java
@@ -1,4 +1,18 @@
 package ru.bmstu.parallel.lab6;
 
-public class StoreActor {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StoreActor extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(Result.class, m -> saveResult(m))
+                .match(String.class, m -> sender().tell(getResult(m), getSelf()))
+                .build();
+    }
 }
