d793bf9d21ef9655071c83d555044f4afd228b46
Baklanov Lev <penachett@bk.ru>
2019-12-11 21:56:20 +0300

    Scripted auto-commit on change (2019-12-11 21:56:20) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/parallel/lab6/SetServersMessage.java
+++ lab6/src/main/java/ru/bmstu/parallel/lab6/SetServersMessage.java
@@ -1,4 +1,16 @@
 package ru.bmstu.parallel.lab6;
 
+import java.util.ArrayList;
+
 public class SetServersMessage {
+
+    private ArrayList<String> servers;
+
+    public ArrayList<String> getServers() {
+        return servers;
+    }
+
+    public void setServers(ArrayList<String> servers) {
+        this.servers = servers;
+    }
 }
--- lab6/src/main/java/ru/bmstu/parallel/lab6/StoreActor.java
+++ lab6/src/main/java/ru/bmstu/parallel/lab6/StoreActor.java
@@ -3,16 +3,19 @@ package ru.bmstu.parallel.lab6;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class StoreActor extends AbstractActor {
 
+    private ArrayList<String> servers;
+
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(GetRandomServerMessage.class, m -> saveResult(m))
-                .match(SetServersMessage.class, m -> sender().tell(getRandomServer(), getSelf()))
+                .match(GetRandomServerMessage.class, m -> sender().tell(getRandomServer(), getSelf()))
+                .match(SetServersMessage.class, m -> setServers())
                 .build();
     }
 
@@ -20,5 +23,7 @@ public class StoreActor extends AbstractActor {
 
     }
 
-    private void 
+    private void setServers(ArrayList<String> servers) {
+
+    }
 }
