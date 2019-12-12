df106897166b91f3683ca1c27a26f6eac4fcc11b
Baklanov Lev <penachett@bk.ru>
2019-12-13 01:22:12 +0300

    Scripted auto-commit on change (2019-12-13 01:22:12) by gitwatch.sh

--- lab7/src/main/java/ru/bmstu/parallel/lab7/Client.java
+++ lab7/src/main/java/ru/bmstu/parallel/lab7/Client.java
@@ -1,11 +1,12 @@
 package ru.bmstu.parallel.lab7;
 
+import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
 
 public class Client {
 
     public static void main(String[] args) {
         ZMQ.Context context = ZMQ.context(1);
-        ZMQ.Socket requester = context.socket()
+        ZMQ.Socket requester = context.socket(SocketType.REQ);
     }
 }
