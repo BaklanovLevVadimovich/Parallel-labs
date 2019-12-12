f3a3de82ba8afcfabed17215286bb14632c2a221
Baklanov Lev <penachett@bk.ru>
2019-12-12 03:05:05 +0300

    Scripted auto-commit on change (2019-12-12 03:05:05) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/parallel/lab6/ZookeeperHandler.java
+++ lab6/src/main/java/ru/bmstu/parallel/lab6/ZookeeperHandler.java
@@ -1,8 +1,16 @@
 package ru.bmstu.parallel.lab6;
 
+import akka.actor.ActorRef;
+import org.apache.zookeeper.ZooKeeper;
+
 public class ZookeeperHandler {
 
-    public ZookeeperHandler() {
-        
+    private ActorRef storageActor;
+    private ZooKeeper zoo;
+    private static final connectString =
+
+    public ZookeeperHandler(ActorRef storageActor) {
+        this.storageActor = storageActor;
+        zoo = new ZooKeeper()
     }
 }
