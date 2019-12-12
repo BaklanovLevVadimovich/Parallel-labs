b56ec683ba93868a39db35044dca047006def66d
Baklanov Lev <penachett@bk.ru>
2019-12-13 02:27:33 +0300

    Scripted auto-commit on change (2019-12-13 02:27:33) by gitwatch.sh

--- lab7/src/main/java/ru/bmstu/parallel/lab7/DataStoreInfo.java
+++ lab7/src/main/java/ru/bmstu/parallel/lab7/DataStoreInfo.java
@@ -1,4 +1,34 @@
 package ru.bmstu.parallel.lab7;
 
 public class DataStoreInfo {
+
+    private String id;
+
+    private int beginRange;
+
+    private int endRange;
+
+    public int getBeginRange() {
+        return beginRange;
+    }
+
+    public int getEndRange() {
+        return endRange;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setBeginRange(int beginRange) {
+        this.beginRange = beginRange;
+    }
+
+    public void setEndRange(int endRange) {
+        this.endRange = endRange;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
