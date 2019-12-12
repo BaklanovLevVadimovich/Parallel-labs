84d3e4f6e4ab00c32a91377cb91cb30ce703df02
Baklanov Lev <penachett@bk.ru>
2019-12-12 03:11:31 +0300

    Scripted auto-commit on change (2019-12-12 03:11:31) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/parallel/lab6/ZookeeperHandler.java
+++ lab6/src/main/java/ru/bmstu/parallel/lab6/ZookeeperHandler.java
@@ -10,6 +10,7 @@ public class ZookeeperHandler {
     private ActorRef storageActor;
     private ZooKeeper zoo;
     private static final String connectString = "localhost:2081";
+    private static final String HOST = "localhost";
     private static final int TIMEOUT_MILLIS = 5000;
 
     public ZookeeperHandler(ActorRef storageActor) throws IOException {
@@ -17,5 +18,7 @@ public class ZookeeperHandler {
         zoo = new ZooKeeper(connectString, TIMEOUT_MILLIS, null);
     }
 
-    public void createServer(int port, )
+    public void createServer(int port) {
+        
+    }
 }
