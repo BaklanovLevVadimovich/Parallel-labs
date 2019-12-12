97fed267b8e8c4b31f6fbcb50ec2b0a00137cf99
Baklanov Lev <penachett@bk.ru>
2019-12-13 01:03:13 +0300

    Scripted auto-commit on change (2019-12-13 01:03:13) by gitwatch.sh

--- lab7/src/main/java/ru/bmstu/parallel/lab7/DataStore.java
+++ lab7/src/main/java/ru/bmstu/parallel/lab7/DataStore.java
@@ -7,19 +7,20 @@ public class DataStore {
     private static HashMap<Integer, String> data;
     private static int rangeBegin;
     private static int rangeEnd;
-    private static final String RANGE_DELIMETER = "-";
-    private static final String VAlUES_DELIMETER = ",";
+    private static final String RANGE_DELIMITER = "-";
+    private static final String VALUES_DELIMITER = ",";
 
     public static void main(String[] args) {
         String range = args[0];
         String values = args[1];
-        String[] rangeSplitted = range.split(RANGE_DELIMETER);
+        String[] rangeSplited = range.split(RANGE_DELIMITER);
         rangeBegin = Integer.parseInt(rangeSplitted[0]);
         rangeEnd = Integer.parseInt(rangeSplitted[1]);
-        String[] valuesSplitted = values.split(VAlUES_DELIMETER);
+        String[] valuesSplitted = values.split(VALUES_DELIMITER);
         for (int i = rangeBegin, j = 0; i <= rangeEnd; i++, j++) {
             data.put(i, valuesSplitted[j]);
         }
-        
+
+
     }
 }
