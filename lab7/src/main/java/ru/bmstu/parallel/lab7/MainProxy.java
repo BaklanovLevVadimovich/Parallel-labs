987d93ad93512a84331e955c5beb7f3831925b89
Baklanov Lev <penachett@bk.ru>
2019-12-13 01:40:18 +0300

    Scripted auto-commit on change (2019-12-13 01:40:18) by gitwatch.sh

--- lab7/src/main/java/ru/bmstu/parallel/lab7/MainProxy.java
+++ lab7/src/main/java/ru/bmstu/parallel/lab7/MainProxy.java
@@ -1,11 +1,13 @@
 package ru.bmstu.parallel.lab7;
 
+import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
 
 public class MainProxy {
 
     public static void main(String[] args) {
         ZMQ.Context context = ZMQ.context(1);
-        ZMQ.Socket clientWorker = context.socket()
+        ZMQ.Socket clientWorker = context.socket(SocketType.ROUTER);
+        ZMQ.Socket storeWorker = context.socket(SocketType.ROUTER);
     }
 }
