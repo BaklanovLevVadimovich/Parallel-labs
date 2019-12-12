bea9d22fdbf5bc3efb5c43f0ed4f793df9bde484
Baklanov Lev <penachett@bk.ru>
2019-12-12 03:06:45 +0300

    Scripted auto-commit on change (2019-12-12 03:06:45) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/parallel/lab6/ZookeeperHandler.java
+++ lab6/src/main/java/ru/bmstu/parallel/lab6/ZookeeperHandler.java
@@ -8,9 +8,10 @@ public class ZookeeperHandler {
     private ActorRef storageActor;
     private ZooKeeper zoo;
     private static final String connectString = "localhost:2081";
+    private static final int TIMEOUT_MILLIS = 5000;
 
     public ZookeeperHandler(ActorRef storageActor) {
         this.storageActor = storageActor;
-        zoo = new ZooKeeper(connectString, )
+        zoo = new ZooKeeper(connectString, TIMEOUT_MILLIS, )
     }
 }
