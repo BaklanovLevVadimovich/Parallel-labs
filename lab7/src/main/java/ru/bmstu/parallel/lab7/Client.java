fa1ff7f6ee7c2da2619dcb00cf88ddbdcd353435
Baklanov Lev <penachett@bk.ru>
2019-12-13 01:24:37 +0300

    Scripted auto-commit on change (2019-12-13 01:24:37) by gitwatch.sh

--- lab7/src/main/java/ru/bmstu/parallel/lab7/Client.java
+++ lab7/src/main/java/ru/bmstu/parallel/lab7/Client.java
@@ -12,7 +12,7 @@ public class Client {
         ZMQ.Socket requester = context.socket(SocketType.REQ);
         requester.connect("tcp://localhost:5559");
         System.out.println("Socket connected");
-        Scanner in = 
+        Scanner in = Scanner(System.in);
         for (;;) {
 
         }
