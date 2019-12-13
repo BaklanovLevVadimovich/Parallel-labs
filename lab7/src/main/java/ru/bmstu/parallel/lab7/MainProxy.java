39a33427ca1c5e0adea72309f622fa8403544881
Baklanov Lev <penachett@bk.ru>
2019-12-13 20:20:12 +0300

    Scripted auto-commit on change (2019-12-13 20:20:12) by gitwatch.sh

--- lab7/src/main/java/ru/bmstu/parallel/lab7/MainProxy.java
+++ lab7/src/main/java/ru/bmstu/parallel/lab7/MainProxy.java
@@ -20,90 +20,90 @@ public class MainProxy {
         ZMQ.Socket storeWorker = context.socket(SocketType.ROUTER);
         clientWorker.bind("tcp://*:8081");
         storeWorker.bind("tcp://*:8082");
-        ZMQ.Poller items = context.poller(2);
-        items.register(clientWorker, ZMQ.Poller.POLLIN);
-        items.register(storeWorker, ZMQ.Poller.POLLIN);
-        System.out.println("Launched proxy");
-        boolean more = false;
-        String message;
-        while (!Thread.currentThread().isInterrupted()) {
-            items.poll();
-            if (items.pollin(0)) {
-                while (true) {
-                    System.out.println("GETTING NEW MESSAGE");
-                    String id = clientWorker.recvStr();
-                    if (isNewClient(id)) {
-                        clientIds.add(id);
-                    }
-                    System.out.println("id:" + id);
-//                    clientWorker.sendMore(id);
-                    clientWorker.recvStr();
-                    message = clientWorker.recvStr();
-                    System.out.println(message);
-//                    clientWorker.sendMore("");
-//                    clientWorker.send("roflan", 0);
-//                    System.out.println("Sended roflan");
-                    if (message.contains("get")) {
-                        String[] messageParts = message.split(REQUEST_DELIMITER);
-                        String storeId = getDataStoreIdContainingCell(Integer.parseInt(messageParts[1]));
-                        System.out.println("SEND GET REQUEST TO DATA STORE " + storeId);
-//                        storeWorker.send(message, 0);
-                        storeWorker.sendMore(storeId);
-                        storeWorker.sendMore("");
-                        System.out.println("SEND MORE PASSED");
-                        storeWorker.send(message + " " + id, 0);
-                        System.out.println("LAST SEND PASSED");
-                    } else {
-
-                    }
-                    more = clientWorker.hasReceiveMore();
-//                    storeWorker.send(message, more ? ZMQ.SNDMORE : 0);
-                    if (!more) {
-                        break;
-                    }
-                }
-            }
-            if (items.pollin(1)) {
-                while (true) {
-                    System.out.println("GETTING NEW STORE MESSAGE");
-                    String id = storeWorker.recvStr(0);
-                    if (isNewStore(id)) {
-                        DataStoreInfo info = new DataStoreInfo();
-                        info.setId(id);
-                        storeInfos.add(info);
-                    }
-                    System.out.println("id: " + id);
-//                    String delim = storeWorker.recvStr();
-//                    System.out.println("delim:" + delim);
-                    message = storeWorker.recvStr(0);
-//                    storeWorker.send(id, 0);
-                    storeWorker.sendMore(id);
-                    storeWorker.sendMore("");
-                    storeWorker.send("ping", 0);
-                    System.out.println("GOT MES FROM STORE " + message);
-                    String[] messageParts = message.split(STORE_MESSAGE_DELIMITER);
-                    if (messageParts[0].equals("NOTIFY")) {
-                        String[] rangeParts = messageParts[1].split(STORE_RANGE_DELIMITER);
-                        int rangeStart = Integer.parseInt(rangeParts[0]);
-                        int rangeEnd = Integer.parseInt(rangeParts[1]);
-                        setNewDataStoreInfo(id, rangeStart, rangeEnd);
-                    }
-                    if (messageParts[0].equals("VALUE")) {
-                        clientWorker.sendMore(messageParts[2]);
-                        clientWorker.send(messageParts[1], 0);
-                    }
-                    if (messageParts[0].equals("UPDATE")) {
-
-                    }
-                    more = storeWorker.hasReceiveMore();
-//                    storeWorker.send(message, more ? ZMQ.SNDMORE : 0);
-                    if (!more) {
-                        break;
-                    }
-                }
-            }
-
-        }
+//        ZMQ.Poller items = context.poller(2);
+//        items.register(clientWorker, ZMQ.Poller.POLLIN);
+//        items.register(storeWorker, ZMQ.Poller.POLLIN);
+//        System.out.println("Launched proxy");
+//        boolean more = false;
+//        String message;
+//        while (!Thread.currentThread().isInterrupted()) {
+//            items.poll();
+//            if (items.pollin(0)) {
+//                while (true) {
+//                    System.out.println("GETTING NEW MESSAGE");
+//                    String id = clientWorker.recvStr();
+//                    if (isNewClient(id)) {
+//                        clientIds.add(id);
+//                    }
+//                    System.out.println("id:" + id);
+////                    clientWorker.sendMore(id);
+//                    clientWorker.recvStr();
+//                    message = clientWorker.recvStr();
+//                    System.out.println(message);
+////                    clientWorker.sendMore("");
+////                    clientWorker.send("roflan", 0);
+////                    System.out.println("Sended roflan");
+//                    if (message.contains("get")) {
+//                        String[] messageParts = message.split(REQUEST_DELIMITER);
+//                        String storeId = getDataStoreIdContainingCell(Integer.parseInt(messageParts[1]));
+//                        System.out.println("SEND GET REQUEST TO DATA STORE " + storeId);
+////                        storeWorker.send(message, 0);
+//                        storeWorker.sendMore(storeId);
+//                        storeWorker.sendMore("");
+//                        System.out.println("SEND MORE PASSED");
+//                        storeWorker.send(message + " " + id, 0);
+//                        System.out.println("LAST SEND PASSED");
+//                    } else {
+//
+//                    }
+//                    more = clientWorker.hasReceiveMore();
+////                    storeWorker.send(message, more ? ZMQ.SNDMORE : 0);
+//                    if (!more) {
+//                        break;
+//                    }
+//                }
+//            }
+//            if (items.pollin(1)) {
+//                while (true) {
+//                    System.out.println("GETTING NEW STORE MESSAGE");
+//                    String id = storeWorker.recvStr(0);
+//                    if (isNewStore(id)) {
+//                        DataStoreInfo info = new DataStoreInfo();
+//                        info.setId(id);
+//                        storeInfos.add(info);
+//                    }
+//                    System.out.println("id: " + id);
+////                    String delim = storeWorker.recvStr();
+////                    System.out.println("delim:" + delim);
+//                    message = storeWorker.recvStr(0);
+////                    storeWorker.send(id, 0);
+//                    storeWorker.sendMore(id);
+//                    storeWorker.sendMore("");
+//                    storeWorker.send("ping", 0);
+//                    System.out.println("GOT MES FROM STORE " + message);
+//                    String[] messageParts = message.split(STORE_MESSAGE_DELIMITER);
+//                    if (messageParts[0].equals("NOTIFY")) {
+//                        String[] rangeParts = messageParts[1].split(STORE_RANGE_DELIMITER);
+//                        int rangeStart = Integer.parseInt(rangeParts[0]);
+//                        int rangeEnd = Integer.parseInt(rangeParts[1]);
+//                        setNewDataStoreInfo(id, rangeStart, rangeEnd);
+//                    }
+//                    if (messageParts[0].equals("VALUE")) {
+//                        clientWorker.sendMore(messageParts[2]);
+//                        clientWorker.send(messageParts[1], 0);
+//                    }
+//                    if (messageParts[0].equals("UPDATE")) {
+//
+//                    }
+//                    more = storeWorker.hasReceiveMore();
+////                    storeWorker.send(message, more ? ZMQ.SNDMORE : 0);
+//                    if (!more) {
+//                        break;
+//                    }
+//                }
+//            }
+//
+//        }
     }
 
     private static boolean isNewClient(String id) {
