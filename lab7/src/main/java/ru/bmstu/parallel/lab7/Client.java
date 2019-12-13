dcfd12ef9e9088d7ed6cc3ae1a4be56d63812bf0
Baklanov Lev <penachett@bk.ru>
2019-12-14 00:39:05 +0300

    Scripted auto-commit on change (2019-12-14 00:39:05) by gitwatch.sh

--- lab7/src/main/java/ru/bmstu/parallel/lab7/Client.java
+++ lab7/src/main/java/ru/bmstu/parallel/lab7/Client.java
@@ -3,27 +3,27 @@ package ru.bmstu.parallel.lab7;
 import org.zeromq.SocketType;
 import org.zeromq.ZFrame;
 import org.zeromq.ZMQ;
-import org.zeromq.ZMsg;
 
 import java.util.Scanner;
 
 public class Client {
 
-    public static void main(String[] args) throws InterruptedException{
-        ZMQ.Context context = ZMQ.context(1);
+    private static final String SOCKET_ADDRESS = "tcp://localhost:8081";
+    private static final int THREADS_NUM = 1;
+    private static final int NO_FLAGS = 0;
+
+    public static void main(String[] args) {
+        ZMQ.Context context = ZMQ.context(THREADS_NUM);
         ZMQ.Socket requester = context.socket(SocketType.REQ);
-        requester.connect("tcp://localhost:8081");
+        requester.connect(SOCKET_ADDRESS);
         Scanner in = new Scanner(System.in);
         System.out.println("Socket connected");
 
         while (!Thread.currentThread().isInterrupted()) {
             String line = in.nextLine();
             ZFrame frame  = new ZFrame(line);
-            frame.send(requester, 0);
-//            requester.send(line, 0);
-            System.out.println("send line: " + line);
-//            ZMsg msg = ZMsg.recvMsg(requester);
-            String reply = requester.recvStr(0);
+            frame.send(requester, NO_FLAGS);
+            String reply = requester.recvStr(NO_FLAGS);
             System.out.println("Got reply: " + reply);
         }
     }
