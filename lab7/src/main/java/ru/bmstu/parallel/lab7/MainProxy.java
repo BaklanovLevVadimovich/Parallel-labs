d15e5638d63b8f59d33f45f092c3823ce1f7a793
Baklanov Lev <penachett@bk.ru>
2019-12-13 14:31:23 +0300

    Scripted auto-commit on change (2019-12-13 14:31:23) by gitwatch.sh

--- lab7/src/main/java/ru/bmstu/parallel/lab7/MainProxy.java
+++ lab7/src/main/java/ru/bmstu/parallel/lab7/MainProxy.java
@@ -34,7 +34,7 @@ public class MainProxy {
                         clientIds.add(id);
                     }
                     System.out.println(id);
-//                    clientWorker.sendMore(id);
+                    clientWorker.sendMore(id);
                     message = clientWorker.recv(0);
                     System.out.println(new String(message));
 //                    clientWorker.send(id.getBytes(), ZMQ.SNDMORE);
