f5901e7a69782913deb6a89f18b430f2d62692a8
Baklanov Lev <penachett@bk.ru>
2019-12-13 14:32:11 +0300

    Scripted auto-commit on change (2019-12-13 14:32:11) by gitwatch.sh

--- lab7/src/main/java/ru/bmstu/parallel/lab7/MainProxy.java
+++ lab7/src/main/java/ru/bmstu/parallel/lab7/MainProxy.java
@@ -43,11 +43,11 @@ public class MainProxy {
 //                    String requestType = lineSplitted[0];
 //                    int cellNum = Integer.parseInt(lineSplitted[1]);
 //                    System.out.println("Type: " + requestType + " | num: " + cellNum);
-                    more = clientWorker.hasReceiveMore();
-                    clientWorker.send(message, more ? ZMQ.SNDMORE : 0);
-                    if (!more) {
-                        break;
-                    }
+//                    more = clientWorker.hasReceiveMore();
+//                    clientWorker.send(message, more ? ZMQ.SNDMORE : 0);
+//                    if (!more) {
+//                        break;
+//                    }
                 }
             }
             if (items.pollin(1)) {
