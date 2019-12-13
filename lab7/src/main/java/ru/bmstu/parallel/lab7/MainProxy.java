a0f4b1a824770f671b822dffaf7d82e815a7d387
Baklanov Lev <penachett@bk.ru>
2019-12-13 20:23:46 +0300

    Scripted auto-commit on change (2019-12-13 20:23:46) by gitwatch.sh

--- lab7/src/main/java/ru/bmstu/parallel/lab7/MainProxy.java
+++ lab7/src/main/java/ru/bmstu/parallel/lab7/MainProxy.java
@@ -39,13 +39,13 @@ public class MainProxy {
                         clientIds.add(id);
                     }
                     System.out.println("id:" + id);
-                    clientWorker.sendMore(id);
+//                    clientWorker.sendMore(id);
                     clientWorker.recvStr();
                     message = clientWorker.recvStr();
                     System.out.println(message);
-                    clientWorker.sendMore("");
-                    clientWorker.send("roflan", 0);
-                    System.out.println("Sended roflan");
+//                    clientWorker.sendMore("");
+//                    clientWorker.send("roflan", 0);
+//                    System.out.println("Sended roflan");
                     if (message.contains("get")) {
                         String[] messageParts = message.split(REQUEST_DELIMITER);
                         String storeId = getDataStoreIdContainingCell(Integer.parseInt(messageParts[1]));
