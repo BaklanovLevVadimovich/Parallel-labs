9f63e532a3cb55ad1f63e6cb09905b03224ad412
Baklanov Lev <penachett@bk.ru>
2019-12-13 21:25:33 +0300

    Scripted auto-commit on change (2019-12-13 21:25:33) by gitwatch.sh

--- lab7/src/main/java/ru/bmstu/parallel/lab7/Client.java
+++ lab7/src/main/java/ru/bmstu/parallel/lab7/Client.java
@@ -1,6 +1,7 @@
 package ru.bmstu.parallel.lab7;
 
 import org.zeromq.SocketType;
+import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
 import java.util.Scanner;
@@ -8,14 +9,12 @@ import java.util.Scanner;
 public class Client {
 
     public static void main(String[] args) {
-        ZMQ.Context context = ZMQ.context(1);
-        ZMQ.Socket requester = context.socket(SocketType.REQ);
+        ZContext context = new ZContext();
+        ZMQ.Socket requester = context.createSocket(SocketType.REQ);
         requester.connect("tcp://localhost:8081");
         Scanner in = new Scanner(System.in);
         System.out.println("Socket connected");
 
-        ZMQ.Poller items = context.poller(1);
-        items.register(requester, ZMQ.Poller.POLLIN);
         while (!Thread.currentThread().isInterrupted()) {
             String line = in.nextLine();
             requester.send(line, 0);
